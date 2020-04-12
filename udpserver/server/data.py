8ea12d55564b2f48ff1656f45835a082e20fdaee
SankhaSDe <sankha.s.de@gmail.com>
2020-04-12 18:27:00 +0100

    updated

--- udpserver/server/data.py
+++ udpserver/server/data.py
@@ -4,19 +4,27 @@ from server.serverlog import debug,error
 
 class Data:
     """
-    Internal data storage is a 2D array(list of lists) that holds information in
-    below format:
-    -----------------------------------------------------------------------
-    |ctype|   cid    |   lat   |   lon   |  update  |is_udp|   ip  | port |
-    -----------------------------------------------------------------------
-    | 1   |1234567890|185185133|185185133|1234567890|   0  |x.x.x.x|765430|
-    -----------------------------------------------------------------------
+    [ _data_index ]
+         ------------------------
+         |     cid    |  index  |
+         ------------------------
+         | 1234567890 |    1    |
+         | 1234567891 |    2    |
+
+    [ _location_data ] internal data storage is a 2D array(list of lists) that
+    holds information in below format:
+         -----------------------------------------------------------------------
+         |ctype|   cid    |   lat   |   lon   |  update  |is_udp|   ip  | port |
+         -----------------------------------------------------------------------
+       1 | 1   |1234567890|185185133|185185133|1234567890|   0  |x.x.x.x|765430|
+       2 | 1   |1234567891|185185133|185185133|1234567890|   0  |x.x.x.x|765430|
+         -----------------------------------------------------------------------
     """
 
     def __init__(self):
         self._location_data = []
         self._data_index = {}
-        
+
     def insert(self,ctype,cid,lat,lon,update,is_udp,ip,port):
         #TODO - validate input
         if cid in self._data_index:
@@ -25,7 +33,7 @@ class Data:
         else:
             self._location_data.append([ctype,cid,lat,lon,update,is_udp,ip,port])
             self._data_index[cid] = len(self._location_data)-1
-    
+
     def search_nearby(self,cid,lat,lon):
         return [x for x in data._location_data if (x[1]!=cid)]
 
